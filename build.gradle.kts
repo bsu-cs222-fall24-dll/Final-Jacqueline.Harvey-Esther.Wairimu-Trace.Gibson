e233b0d27112d24b3368ea08931a1aec366581d7
Estherwairimu <100499476+Estherwairimu@users.noreply.github.com>
2024-11-01 14:31:56 -0400

    Added Sqlite JDBC driver to build.gradle.kts dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.xerial:sqlite-jdbc:3.47.0.0")
+
 }
 
 tasks.test {
