1e14065e564ef5552a641f695e523530c8940a11
Estherwairimu <100499476+Estherwairimu@users.noreply.github.com>
2024-11-13 15:06:32 -0500

    I added implementations to the dependencies, plugins, application and JavaFX to our project’s build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0" // Ensure the plugin version is correct
 }
 
-group = "org.example"
+group = "edu.bsu.cs"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -13,13 +15,19 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.xerial:sqlite-jdbc:3.47.0.0")
-
+    implementation("org.slf4j:slf4j-nop:2.0.11")
     implementation("com.jayway.jsonpath:json-path:2.9.0")
     implementation("net.minidev:json-smart:2.5.0")
-    implementation("org.slf4j:slf4j-nop:2.0.11")
-
+    implementation("org.json:json:20210307")
+    testImplementation("org.testfx:testfx-junit5:4.0.15-alpha")
 }
-
+javafx {
+    version = "22"
+    modules("javafx.controls", "javafx.fxml")
+}
+application {
+    mainClass.set("edu.bsu.cs.LibraryCatalogGUI")
+}
 tasks.test {
     useJUnitPlatform()
 }
