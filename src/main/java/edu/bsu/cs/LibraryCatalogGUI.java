1815ff674917bf1ae41f5e054fb26cd53f6f2d41
Estherwairimu <100499476+Estherwairimu@users.noreply.github.com>
2024-11-13 18:06:22 -0500

    I added a VBox layout with 10-pixel spacing in the LibraryCatalogGUI class. Though the scene now includes this layout, it’s still empty for our third iteration.

--- src/main/java/edu/bsu/cs/LibraryCatalogGUI.java
+++ src/main/java/edu/bsu/cs/LibraryCatalogGUI.java
@@ -2,6 +2,8 @@ package edu.bsu.cs;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 
 public class LibraryCatalogGUI extends Application {
 
@@ -9,8 +11,13 @@ public class LibraryCatalogGUI extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Simple Library GUI");
         primaryStage.show();
+        // Create a VBox layout with spacing of 10
+        VBox layout = new VBox(10);
+        // Set up the scene and add the layout
+        Scene scene = new Scene(layout, 300, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
