7c05799058918bf451c321c255ab2ba271544734
Esther Wairimu <100499476+Estherwairimu@users.noreply.github.com>
2024-11-26 10:18:51 -0500

    Update LibraryCatalogGUI.java
    Removed Unnecessary comments

--- src/main/java/edu/bsu/cs/LibraryCatalogGUI.java
+++ src/main/java/edu/bsu/cs/LibraryCatalogGUI.java
@@ -1,34 +1,31 @@
-//package edu.bsu.cs;
-//import javafx.application.Application;
-//import javafx.scene.Scene;
-//import javafx.scene.control.*;
-//import javafx.scene.layout.VBox;
-//import javafx.stage.Stage;
-//
-//public class LibraryCatalogGUI extends Application {
-//
-//    @Override
-//    public void start(Stage primaryStage) {
-//        // Account setup fields
-//        TextField usernameField = new TextField();
-//        usernameField.setPromptText("Enter Username");
-//
-//        PasswordField passwordField = new PasswordField();
-//        passwordField.setPromptText("Enter Password");
-//
-//        Button createAccountButton = new Button("Create Account");
-//
-//        // Layout to add components
-//        VBox layout = new VBox(10, usernameField, passwordField, createAccountButton);
-//        Scene scene = new Scene(layout, 300, 200);
-//
-//        // Window settings
-//        primaryStage.setTitle("Library Catalog - Account Setup");
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-//    }
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//}
+package edu.bsu.cs;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class LibraryCatalogGUI extends Application {
+
+   @Override
+   public void start(Stage primaryStage) {
+       TextField usernameField = new TextField();
+       usernameField.setPromptText("Enter Username");
+
+       PasswordField passwordField = new PasswordField();
+       passwordField.setPromptText("Enter Password");
+
+       Button createAccountButton = new Button("Create Account");
+     
+       VBox layout = new VBox(10, usernameField, passwordField, createAccountButton);
+       Scene scene = new Scene(layout, 300, 200);
+     
+       primaryStage.setTitle("Library Catalog - Account Setup");
+       primaryStage.setScene(scene);
+       primaryStage.show();
+   }
+
+   public static void main(String[] args) {
+       launch(args);
+   }
+}
